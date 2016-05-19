76bba48e020fb169c779bf10b6724184a7c79e70
Petar Petrov <pppepito86@gmail.com>
2016-05-20 02:13:02 +0300

    ubuntu setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt-get install git -y
+apt-get install nodejs -y
+
+# mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+mkdir -p ~/nodejsserver
+cd ~/nodejsserver
+
+git clone https://github.com/pppepito86/lego_scoring_rest.git --depth=1
+
+apt-get install npm -y
+npm install
+
+mkdir -p /data/db
+chmod -R 777 /data/db/
+mongod &
+
+https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y build-essential
+apt-get install nodejs-legacy
+
+cd lego_scoring_rest
+node app.js &
+
+curl http://localhost:8085/load
